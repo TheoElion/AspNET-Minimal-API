9b83eaaf93579ce4e89fb71de5365fd3eae9ff31
Theo Elion <theoelion54@gmail.com>
2025-04-09 15:43:09 -0300

    Adicionar arquivos de projeto.

---
+++ Entitites/Ingrediente.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml;
+
+namespace RangoAgilApi.Entitites;
+
+public class Ingrediente
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public required string Nome { get; set; }
+
+    public ICollection<Rango> Rangos { get; set; } = [];
+
+    public Ingrediente()
+    {
+            
+    }
+
+    [SetsRequiredMembers]
+    public Ingrediente(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
